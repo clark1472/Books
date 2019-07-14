2596c3f93a7507872d2773bc6ab9dfebcb4df45c
Salsa <>
2019-07-13 21:25:23 -0700

    what a mess

--- models/book.js
+++ models/book.js
@@ -4,8 +4,10 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
-  description: String,
-  date: { type: Date, default: Date.now }
+  description: { type: String },
+  //date: { type: Date, default: Date.now },
+  image: { type: String, required: true },
+  link: { type: String, required: true }
 });
 
 const Book = mongoose.model("Book", bookSchema);
